12c83615bfbcdeb9c43c792ab64aa0f1abea05ba
Lars Shakya buch-Jepsen <lbj@applied-intelligence.dk>
2017-12-06 09:51:33 +0100

    Removed database version stored procedures as they are irrelevant to project

--- Pre2016/TableChange/SelectDatabaseVersion.StoredProcedure.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE dbo.SelectDatabaseVersion
-AS
-BEGIN
-	SET NOCOUNT ON
-
-	SELECT CAST(ISNULL(V.value,'') AS VARCHAR(255)) AS 'Version', CAST(ISNULL(U.value,'') AS VARCHAR(1024)) AS 'UpdatedBy'
-						FROM fn_listextendedproperty('Version', default, default, default, default, default, default) V,
-						fn_listextendedproperty('UpdatedBy', default, default, default, default, default, default) U
-END
